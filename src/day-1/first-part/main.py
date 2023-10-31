164728788b35f8259b0656ad95b970c7a07fd670
Moonrise <70655051+PetitPotiron@users.noreply.github.com>
2023-10-31 19:29:22 +0100

    Update main.py

--- src/day-1/first-part/main.py
+++ src/day-1/first-part/main.py
@@ -1,8 +1,8 @@
 floor = 0
 with open("input.txt","r") as file:
-    for letter in file.read():
-        if letter == "(":
+    for instruction in file.read():
+        if instruction == "(":
             floor += 1
         else:
             floor -= 1
-print("The Santa will finish on the floor", str(floor) + ".")
+print("The Santa will finish on the floor " + str(floor) + ".")
