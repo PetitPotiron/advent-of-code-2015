2884fd19a5775cd1d8ae51d3da6efc60ffcb6909
Moonrise <70655051+PetitPotiron@users.noreply.github.com>
2023-10-31 19:32:34 +0100

    Update main.py

--- src/day-1/second-part/main.py
+++ src/day-1/second-part/main.py
@@ -1,10 +1,10 @@
 floor = 0
 with open("input.txt","r") as file:
-    for index, letter in enumerate(file.read()):
-        if letter == "(":
+    for position, instruction in enumerate(file.read()):
+        if instruction == "(":
             floor += 1
         else:
             floor -= 1
         if floor == -1:
-            print("The character at position", index+1, "made the santa enter the basement.")
+            print("The instruction at position", index+1, "made the santa enter the basement.")
             break
