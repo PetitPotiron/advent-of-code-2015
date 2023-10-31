39fe927f986258373578bd204e58f667e7ef9230
Moonrise <70655051+PetitPotiron@users.noreply.github.com>
2023-10-31 20:16:29 +0100

    Update index.js

--- src/day-2/first-part/index.js
+++ src/day-2/first-part/index.js
@@ -1,22 +1,19 @@
 const fs = require('fs');
 
 let boxes = [];
-let total_square_feet = 0;
+let total_area = 0;
 
-
-fs.readFile('./input.txt', 'utf8' , (err, data) => {
-    boxes = data.split("\n").map(value => value.replace('\r', '').split("x").map(value => Number(value)));
-    boxes.forEach(box => {
-        let lowest = Infinity;
-        let dimensions = [box[0]*box[1]*2 , box[1]*box[2]*2, box[0]*box[2]*2];
-        for (dimension of dimensions) {
-            if (dimension/2 < lowest) {
-                lowest = dimension/2;
-            }
-        }
-        dimensions.push(lowest);
-        dimensions = dimensions[0] + dimensions[1] + dimensions[2] + dimensions[3];
-        total_square_feet += dimensions;
-    })
-    console.log("The elves should order", total_square_feet, "square feet of wrapping paper.");
+fs.readFile('./input.txt', 'utf8' , (err, boxes_dimensions_list) => {
+    boxes = boxes_dimensions_list.split("\n");
+    for (box of boxes){
+        var dimensions = box.split("x").map(dimension => Number(dimension));
+        var side1 = dimensions[0]*dimensions[1];
+        var side2 = dimensions[0]*dimensions[2];
+        var side3 = dimensions[1]*dimensions[2];
+        total_area += 2*side1
+        total_area += 2*side2
+        total_area += 2*side3
+        total_area += Math.min(side1, side2, side3)
+    }
+console.log("The elves should order", total_area, "square feet of wrapping paper.");
 })
